509e39a00e7ac6bdf910b6424295b3b9657d51d4
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-18 22:48:45 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -19,7 +19,7 @@ def meals
  end
 #########
  def waiters
-   Waiter.all.select {|waiter|waiter.customer.meal  == self.meal}
+   Waiter.all.select {|waiter|waiter.customer  == self.meal}
  end
 ########
 end
