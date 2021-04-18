1e8bea1a75a92d1260f904996a0615e81ad6804d
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-18 21:25:04 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,30 @@
+require 'pry'
+
 class Customer
-  
+  attr_accessor :name, :age, :waiter, :total, :tip, :cutomer, :years, :meal
+#  attr_reader  :customer, :years
+  @@all = []
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+ def new_meal(waiter, total, tip)
+Meal.new(waiter, self, total, tip)
+ end
+def meals
+ Meal.all.select {|meal| meal.customer == self}
+ end
+ #######################
+ # returns an Array of Waiter instances
+ # associated
+ # with this customer's meals
+
+ def waiters
+   Waiter.all.select {|waiter|waiter.customer == self}
+ end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,34 @@
 class Waiter
-
+  attr_accessor :name, :age, :customer, :total, :tip, :waiter, :years, :meal
+#attr_reader :waiter, :years
+  @@all = []
+def initialize(name, years)
+  @name = name
+  @years = years
+  @@all << self
+end
+def self.all
+  @@all
+end
+ def new_meal(customer, total, tip)
+  Meal.new(self, customer, total, tip)# #initializes a meal using the current Waiter instance, a provided Customer instance and a total and tip
+ end
+ def meals
+  Meal.all.select do |meal| meal.waiter == self
+   end
+end
+#####
+ # returns the Customer instance associated
+ # with the
+ # meal that received the largest tip
+def best_tipper
+#  Meal.all.max_by do |tip| tip.field end
+ Customer.all.max {|a,b| a.attr <=> b.attr }
+  # max_value = 0
+  # Meal.all.each do |tipper|
+  #   if tipper > max_value
+  #   max_value  == tipper
+  #    end
+  # end
+end
 end
