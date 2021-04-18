fd0a8611cab690fa4b0dd098dcbd582dfa3ebc11
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-18 23:28:37 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,6 +1,5 @@
 class Waiter
   attr_accessor :name, :age, :customer, :total, :tip, :waiter, :years, :meal
-#attr_reader :waiter, :years
   @@all = []
 def initialize(name, years)
   @name = name
@@ -11,24 +10,15 @@ def self.all
   @@all
 end
  def new_meal(customer, total, tip)
-  Meal.new(self, customer, total, tip)# #initializes a meal using the current Waiter instance, a provided Customer instance and a total and tip
+  Meal.new(self, customer, total, tip)
  end
  def meals
   Meal.all.select do |meal| meal.waiter == self
    end
 end
 #####
- # returns the Customer instance associated
- # with the
- # meal that received the largest tip
 def best_tipper
-#  Meal.all.max_by do |tip| tip.field end
- Customer.all.max {|a,b| a.attr <=> b.attr }
-  # max_value = 0
-  # Meal.all.each do |tipper|
-  #   if tipper > max_value
-  #   max_value  == tipper
-  #    end
-  # end
+ #Customer.all.max {|a,b| a.attr <=> b.attr }
+ best_tipped_meal = meals.max do |meal_a, meal_b| meal_a.tip <=> meal_b.tip end
 end
 end
